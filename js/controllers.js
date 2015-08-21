e34efe50070352517c5c53e0505f651c176e5622
Chris Hilleren <chilleren@gmail.com>
2015-08-20 22:15:31 -0700

    add taxes, discounts and totalling

--- js/controllers.js
+++ js/controllers.js
@@ -7,7 +7,51 @@ var controllers = angular.module('controllers', []);
 controllers.controller('InvoiceCtrl', ['$scope', function($scope) {
   $scope.invoiceNumber = 10001;
   $scope.invoiceDate = new Date();
-  $scope.invoiceTotal = 0;
+  $scope.subtotal = 0;
+
+  $scope.lineItems = [
+    {name: "Popcorn", description: "Genuine gourmet popping corn", quantity: 3, price: "9.99"},
+    {name: "Bags", description: "Large popcorn bags", quantity: 10, price: "3"}
+  ];
+
+  $scope.taxes = [
+    {type: "Sales", rate: .09},
+    {type: "VAT", rate: .12}
+  ];
+
+  $scope.discounts = [10, 5];
+
+  function sumLineItems() {
+    $scope.subtotal = 0;
+
+    for (var i = 0; i < $scope.lineItems.length; i++) {
+      var lineItem = $scope.lineItems[i];
+      $scope.subtotal += lineItem.price * lineItem.quantity;
+    }
+  }
+
+  sumLineItems();
+
+  $scope.addLineItem = function () {
+    $scope.lineItems.push(
+    {name: "New Item", description: "Details go here", quantity: 2, price: "10"});
+    sumLineItems();
+  }
+
+  $scope.total = function () {
+    var taxTotal = 0;
+    var discountTotal = 0;
+
+    for (var i = 0; i < $scope.taxes.length; i++) {
+      taxTotal += $scope.taxes[i].rate * $scope.subtotal;
+    }
+
+    for (var i = 0; i < $scope.discounts.length; i++) {
+      discountTotal += $scope.discounts[i];
+    }
+
+    return $scope.subtotal + taxTotal - discountTotal;
+  }
 }]);
 
 controllers.controller('AddressCtrl', ['$scope', function($scope) {
@@ -21,13 +65,4 @@ controllers.controller('AddressCtrl', ['$scope', function($scope) {
 }]);
 
 controllers.controller('LineItemListCtrl', ['$scope', function($scope) {
-  $scope.lineItems = [
-    {name: "Popcorn", description: "Genuine gourmet popping corn", quantity: 3, price: "9.99"},
-    {name: "Bags", description: "Large popcorn bags", quantity: 10, price: "3"}
-  ]
-
-  $scope.addLineItem = function () {
-    $scope.lineItems.push(
-    {name: "New Item", description: "Details go here", quantity: 2, price: "10"})
-  }
 }]);
