83f7358c644d92634defce12c9b146dd84275e74
Chris Hilleren <chilleren@gmail.com>
2015-08-26 21:05:35 -0700

    address form validations, invoice number data binding, better styles

--- js/app.js
+++ js/app.js
@@ -7,3 +7,14 @@ var invoiceApp = angular.module('invoiceApp', [
   'controllers.address',
   'directives'
 ]);
+
+
+$(document).on("click", function (ev) {
+  if (!$(ev.target).closest('.highlight').length) {
+    var highlights = $(".highlight");
+    if (highlights) {
+      highlights.removeClass("highlight");
+      ev.stopPropagation();
+    }
+  }
+});
--- js/controllers/address-ctrl.js
+++ js/controllers/address-ctrl.js
@@ -16,6 +16,16 @@ angular.module('controllers.address', [])
     $(event.currentTarget).addClass("highlight");
   }
 
+  $scope.blurInput = function () {
+    var target = $(event.currentTarget);
+    if (target.val().length === 0) {
+      target.tooltip({ trigger: "manual" });
+      target.tooltip("show");
+    } else {
+      target.tooltip("hide");
+    }
+  }
+
 
   // $(document).on("click", function (ev) {
   //   if (!$(ev.target).closest('.highlight').length) {
